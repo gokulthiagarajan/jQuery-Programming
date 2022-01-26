c418b99dd712b9c8bc6c3ab1f447693a312bb403
Gokul Thiagarajan <33049124+gokulthiagarajan@users.noreply.github.com>
2022-01-26 17:14:09 +0530

    Added Selectors, Attributes
    Added Selectors, Attributes

---
+++ 1] Introduction/Document Ready/custom.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(document).ready(function () {
+  console.log("I am ready");
+});
+
+// Anyone fix?
+jquery(document).ready(function () {
+  console.log("I am ready now!");
+});
+
+/*$( function( )
+{
+	console.log("I am ready");
+} );*/
